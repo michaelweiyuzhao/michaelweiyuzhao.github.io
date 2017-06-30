181a577491fefb924e2994e27b871f820df29ba6
Michael Zhao <mweiyuzhao@gmail.com>
2017-06-29 20:14:36 -0400

    slight changes

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
     $("#PCICalc-go").click(function() {
       var newPCI = $("#PCICalc-selecter").val();
       if(newPCI === "4 Attributes with Precipitation") {
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalculator/");
+        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc4P/");
       }
       else if(newPCI === "4 Attributes with M & R") {
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalcT4/");
+        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc4MR/");
       }
       else if(newPCI === "8 Basic Attributes"){
         $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalc8Basic/");
       }
       else {
-        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/");
+        $("#PCICalc-go").attr("href", "https://michaelweiyuzhao.github.io/PCICalcHub");
       }
     });
 });
